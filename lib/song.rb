a20589afebff8de0053c900cd04059ec0b562702
Robert Tate <btate712@gmail.com>
2019-03-14 12:46:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,4 +4,12 @@ class Song
   def initialize(song_name)
     @name = song_name
   end
+
+  def self.new_by_filename(file_name)
+    song_info = file_name.split(" - ")
+    binding.pry
+    new_song = self.new(song_info[1])
+    new_song.artist = song_info[0]
+    new_song.genre = song_info[2].split(".")[0]
+  end
 end
